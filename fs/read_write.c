79ecddad559b21f329fe58f2f9d914682fd62ec5
zhuzhuzihan <68634388+zhuzhuzihan@users.noreply.github.com>
2026-03-20 23:50:33 +0800

    Add files via upload

--- fs/read_write.c
+++ fs/read_write.c
@@ -456,18 +456,9 @@ ssize_t __vfs_read(struct file *file, char __user *buf, size_t count,
 }
 EXPORT_SYMBOL(__vfs_read);
 
-#ifdef CONFIG_KSU
-extern bool ksu_vfs_read_hook __read_mostly;
-extern int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,
-			size_t *count_ptr, loff_t **pos);
-#endif
 ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 {
 	ssize_t ret;
-	   #ifdef CONFIG_KSU
-		if (unlikely(ksu_vfs_read_hook))
-				ksu_handle_vfs_read(&file, &buf, &count, &pos);
-	   #endif
 
 	if (!(file->f_mode & FMODE_READ))
 		return -EBADF;
@@ -589,20 +580,12 @@ static inline void file_pos_write(struct file *file, loff_t pos)
 	if ((file->f_mode & FMODE_STREAM) == 0)
 		file->f_pos = pos;
 }
-#ifdef CONFIG_KSU_MANUAL_HOOK
-extern bool ksu_init_rc_hook __read_mostly;
-extern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd,
-				char __user **buf_ptr, size_t *count_ptr);
-#endif
 
 SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 {
 	struct fd f = fdget_pos(fd);
 	ssize_t ret = -EBADF;
-#ifdef CONFIG_KSU_MANUAL_HOOK
-	if (unlikely(ksu_init_rc_hook)) 
-		ksu_handle_sys_read(fd, &buf, &count);
-#endif
+
 	if (f.file) {
 		loff_t pos = file_pos_read(f.file);
 		ret = vfs_read(f.file, buf, count, &pos);
